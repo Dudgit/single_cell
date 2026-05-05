39fde9be205e67eb99ca4a7e45e584723608eda2
Dudás Bence <52243953+Dudgit@users.noreply.github.com>
2026-05-05 14:23:58 +0200

    Update eval.sh to execute REGINA evaluation
    Replaced the evaluation job for REGINA with a direct call to the singularity container.

--- eval.sh
+++ eval.sh
@@ -1,18 +1,2 @@
 #!/bin/bash
-#SBATCH --job-name=eval_regina
-#SBATCH --partition=ai             
-#SBATCH --gres=gpu:1               
-#SBATCH --ntasks=1                
-#SBATCH --cpus-per-task=32         
-#SBATCH --mem=128G                  
-#SBATCH --time=12:00:00            
-#SBATCH --output=logs/evaluate_regina.log
-
-module load singularity
-export WORKSPACE="/home/c_ai4db/c_ai4sci_scratch/vcell"
-export REAL_WORKSPACE=$(readlink -f $WORKSPACE)
-
-export SINGULARITYENV_PYTHONPATH=$REAL_WORKSPACE
-cd $REAL_WORKSPACE
-
-singularity exec --nv --pwd $REAL_WORKSPACE -B $REAL_WORKSPACE utils/vcell.sif python3 evaluate_h5ad.py
+singularity exec --nv REGINA.sif python3 evaluate_h5ad.py
